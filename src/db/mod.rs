57059bdee0396b5ed2fd5c076045bc71318544fb
Sasha Yusuk <thepulticula@gmail.com>
2020-12-07 21:16:02 +0200

    General refactoring; Integrated r2d2 connection pool

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,75 +1,53 @@
+use diesel::{
+    expression::dsl::{count, exists},
+    insert_into, select,
+    sqlite::SqliteConnection,
+    BoolExpressionMethods, ExpressionMethods, QueryDsl, RunQueryDsl,
+};
+use r2d2::Pool;
+use r2d2_diesel::ConnectionManager;
+use rand::rngs::OsRng;
 use rsa::{pem, PaddingScheme, PublicKey, RSAPrivateKey, RSAPublicKey};
 
-use rand::rngs::OsRng;
+use actix_web::web;
 
-use diesel::expression::dsl::exists;
-use diesel::sqlite::SqliteConnection;
-use diesel::QueryDsl;
-use diesel::{insert_into, select, Connection};
-use diesel::{BoolExpressionMethods, ExpressionMethods, RunQueryDsl};
-
-use std::convert::TryFrom;
-use std::fs::File;
-use std::io::{self, Read};
-use std::string::String;
-use std::sync::{Arc, Mutex};
+use std::{convert::TryFrom, fs::File, io::Read, ops::Deref};
 
 pub mod model;
 pub mod schema;
 
-use schema::users;
+use schema::{tests, users};
 
-lazy_static! {
-    static ref DB: Arc<Mutex<anyhow::Result<SqliteConnection>>> = {
-        let db = extablish_connection_impl();
-        Arc::new(Mutex::new(db))
-    };
+use model::UserForm;
+
+pub type DbPool = Pool<ConnectionManager<SqliteConnection>>;
+
+pub fn establish_connection() -> DbPool {
+    let database_url = "../../drive_tests_db";
+    let manager = ConnectionManager::<SqliteConnection>::new(database_url);
+    r2d2::Pool::builder().build(manager).expect("Failed to crate DB pool")
 }
 
-pub fn extablish_connection() -> anyhow::Result<()> {
-    let db = Arc::clone(&DB);
-
-    let db = &*db.lock().unwrap();
-    match db {
-        Ok(_) => Ok(()),
-        Err(err) => Err(io::Error::new(
-            io::ErrorKind::NotConnected,
-            format!("Failed to connect to the DataBase due to: {:?}", err),
-        )
-        .into()),
-    }
-}
-
-fn extablish_connection_impl() -> anyhow::Result<SqliteConnection> {
-    SqliteConnection::establish("../../drive_tests_db").map_err(|err| err.into())
-}
-
-pub fn registar_new_user(
-    user_name: &str,
-    user_second_name: &str,
-    user_password: &str,
-) -> anyhow::Result<()> {
+pub fn registry_new_user(user: UserForm, pool: web::Data<DbPool>) -> anyhow::Result<()> {
     use self::users::dsl::*;
 
-    if check_if_user_exists(user_name, user_second_name)? {
+    if check_if_user_exists(user.clone(), pool.clone())? {
         return Err(anyhow!(format!(
-            "{} {} user exists",
-            user_name, user_second_name
+            "{} {} user already exists",
+            user.name, user.second_name
         )));
     }
 
-    let encrypted_password = encrypted_password(user_password.to_string())?;
+    let encrypted_password = encrypted_password(user.password.clone())?;
 
-    let db = Arc::clone(&DB);
-    let db = &*db.lock().unwrap();
-
+    let db = pool.get().unwrap();
     let insert_result = insert_into(users)
         .values((
-            name.eq(user_name.to_string()),
-            second_name.eq(user_second_name.to_string()),
+            name.eq(user.name),
+            second_name.eq(user.second_name),
             password.eq(encrypted_password),
         ))
-        .execute(db.as_ref().unwrap());
+        .execute(db.deref());
 
     match insert_result {
         Ok(0) => Err(anyhow!("Failed to insert a row to the Users table")),
@@ -78,15 +56,15 @@ pub fn registar_new_user(
     }
 }
 
-pub fn check_if_user_exists(user_name: &str, user_second_name: &str) -> anyhow::Result<bool> {
+pub fn check_if_user_exists(user: UserForm, pool: web::Data<DbPool>) -> anyhow::Result<bool> {
     use self::users::dsl::*;
-    let db = Arc::clone(&DB);
-    let db = db.lock().unwrap();
 
-    select(exists(users.filter(
-        (name.eq(user_name)).and(second_name.eq(user_second_name)),
-    )))
-    .get_result(db.as_ref().unwrap())
+    let db = pool.get().unwrap();
+
+    select(exists(
+        users.filter((name.eq(user.name)).and(second_name.eq(user.second_name))),
+    ))
+    .get_result(db.deref())
     .map_err(|err| err.into())
 }
 
@@ -129,26 +107,60 @@ fn decrypt_password(encrypted_password_hex: String) -> anyhow::Result<String> {
     Ok(decrypted_password)
 }
 
-pub fn verify_password(
-    user_name: &str,
-    user_second_name: &str,
-    user_password: String,
-) -> anyhow::Result<(bool, i32)> {
+pub fn verify_password(user: UserForm, pool: web::Data<DbPool>) -> anyhow::Result<(bool, i32)> {
     use self::users::dsl::*;
-    let db = Arc::clone(&DB);
-    let db = db.lock().unwrap();
+
+    let db = pool.get().unwrap();
 
     let selected_user: model::User = users
         .order(id)
-        .filter((name.eq(user_name)).and(second_name.eq(user_second_name)))
-        .first::<model::User>(db.as_ref().unwrap())
+        .filter((name.eq(user.name.clone())).and(second_name.eq(user.second_name.clone())))
+        .first::<model::User>(db.deref())
         .map_err(|err| anyhow!("failed to find in the DB User - {}", err))?;
 
     let decrypted_password = decrypt_password(selected_user.password)?;
 
-    if decrypted_password == user_password {
+    if decrypted_password == user.password {
         Ok((true, selected_user.scores))
     } else {
         Ok((false, 0))
     }
+}
+pub fn get_test(pool: web::Data<DbPool>) -> anyhow::Result<model::Test> {
+    use self::tests::dsl::*;
+
+    let db = pool.get().unwrap();
+    let count = tests
+        .select(count(id))
+        .execute(db.deref())
+        .map_err(|err| anyhow!("failed to get tests count - {}", err))?;
+
+    let rand_test = rand::random::<usize>() % count;
+
+    let test = tests
+        .order(id)
+        .filter(id.eq(rand_test as i32))
+        .first::<model::Test>(db.deref())
+        .map_err(|err| anyhow!("failed to get rand test - {}", err))?;
+    Ok(test)
+}
+
+pub fn update_scores(user: &UserForm, new_scores: u32, pool: &web::Data<DbPool>) -> anyhow::Result<()> {
+    use self::users::dsl::*;
+    let db = pool.get().unwrap();
+
+    let mut selected_user: model::User = users
+        .order(id)
+        .filter((name.eq(user.name.clone())).and(second_name.eq(user.second_name.clone())))
+        .first::<model::User>(db.deref())
+        .map_err(|err| anyhow!("failed to find in the DB User - {}", err))?;
+
+    selected_user.scores += new_scores as i32;
+
+    diesel::update(users.filter(id.eq(selected_user.id)))
+        .set(scores.eq(selected_user.scores))
+        .execute(db.deref())
+        .map_err(|err| anyhow!("failed to update user scores - {}", err))?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,37 @@
-use actix_web::{App, HttpServer};
-use driving_tests_site::{db, web};
+use actix_web::{
+    http,
+    middleware::{self, Logger},
+    App, HttpServer,
+};
 use log::info;
-
 use simple_logger::SimpleLogger;
 
 use std::env;
 
+use driving_tests_site::{db, web};
+
 #[actix_web::main]
 async fn main() -> anyhow::Result<()> {
-    let server_addr = env::args()
-        .nth(1)
-        .unwrap_or_else(|| "127.0.0.1:5050".to_string());
+    let server_addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:5050".to_string());
 
     SimpleLogger::new().init().unwrap();
 
-    db::extablish_connection()?;
+    let connection_pool = db::establish_connection();
 
     let tls_builder = web::tls_builder()?;
 
     info!("Successfully connected to the DB");
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .wrap(Logger::new("%a %t %r %b %s %T"))
+            .wrap(middleware::Compress::new(http::ContentEncoding::Identity))
+            .data(connection_pool.clone())
             .service(web::sing_in)
             .service(web::sing_up)
+            .service(web::get_result_with_user)
+            .service(web::get_test)
+            .service(web::get_result)
             .service(web::healthy)
     })
     .bind_openssl(server_addr, tls_builder)?
